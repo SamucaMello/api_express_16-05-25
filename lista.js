e5469169aee4eada5cd02e78b25dd1bdc5533686
SamucaMello <samuel.mellomail@gmail.com>
2025-05-16 19:23:46 +0000

    funcoes da lista

--- lista.js
+++ lista.js
@@ -2,7 +2,6 @@ export let lista = []
 
 export const setUserToList = (name) => {
     let id = lista.length + 1
-    let userObject = {id, name}
-    lista.push(userObject)
+    lista.push({id, name})
 }
 
